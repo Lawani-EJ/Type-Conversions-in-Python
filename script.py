478fda962f81440061ab307fa0607a52a8edd198
Lawani-EJ <elyonjohn60@gmail.com>
2024-05-09 02:49:34 +0100

    Type COnversion When running the code at first with the wrong syntax it displays an error, In Fixing the Error mods were made, Such that the printout runs without errors, For that the use of the str function was applied, This converts the variables savings and total savings to strings The result then is printed out Converted the variable piString to a float as a new variable piFloat and printed out the result.

---
+++ script.py
@@ -0,0 +1,13 @@
+#Define savings and total_savings
+savings = 100
+total_savings = 150
+
+#Fixing the PrintOut
+print("I started With $" + str(savings) + " And now have $" + str(total_savings) + " AWESOME!")
+
+#Definition of pi_string
+pi_string = "3.1415926"
+
+#Converting the pi_string into float: pi_float
+pi_float = float(pi_string)
+print(pi_float)
