88a88655600825419350c9946e087d1e2a9ca041
Lawani-EJ <elyonjohn60@gmail.com>
2024-05-10 22:52:25 +0100

    Types Learning how to conver a number to a boolean, In python, and then printed out the result.

--- script.py
+++ script.py
@@ -2,6 +2,8 @@
 savings = 100
 total_savings = 150
 
+#Wrong Code
+#print("I started with $" + savings + " and now have $" + total_savings + ". Awesome!")
 #Fixing the PrintOut
 print("I started With $" + str(savings) + " And now have $" + str(total_savings) + " AWESOME!")
 
@@ -11,3 +13,10 @@ pi_string = "3.1415926"
 #Converting the pi_string into float: pi_float
 pi_float = float(pi_string)
 print(pi_float)
+
+#Definition of number 
+number = 1
+
+#COnverting the number 1 to a boolean
+boolean = bool(number)
+print(boolean)
